684c81bb24e7a7d47a7ae986ee454370087b2374
Mika <lymichael94@gmail.com>
2017-10-10 11:06:40 +0200

    update icons of menu

--- MyApplication/app/src/main/java/com/example/micha/permittowork/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/micha/permittowork/MainActivity.java
@@ -25,6 +25,9 @@ public class MainActivity extends AppCompatActivity
 
     }
 
+    /**
+     * Init menu
+     */
     private void initNavigationView() {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -55,13 +58,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.permissions) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.safemap) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.documents) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.contact) {
 
         }
 

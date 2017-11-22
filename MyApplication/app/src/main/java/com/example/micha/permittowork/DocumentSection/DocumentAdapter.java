a8d15cc872deab277820ffc91301ca307fca4915
michael.ly <lymichael94@gmail.com>
2017-11-22 14:51:59 +0100

    Add recycler view + display files

---
+++ MyApplication/app/src/main/java/com/example/micha/permittowork/DocumentSection/DocumentAdapter.java
@@ -0,0 +1,74 @@
+package com.example.micha.permittowork.DocumentSection;
+
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.micha.permittowork.Entity.Document;
+import com.example.micha.permittowork.R;
+
+import java.util.List;
+
+/**
+ * Created by micha on 22/11/2017.
+ */
+public class DocumentAdapter extends RecyclerView.Adapter<DocumentAdapter.CellViewHolder> {
+
+    private List<Document> documentList;
+
+    DocumentAdapter(List<Document> documentList) {
+        this.documentList = documentList;
+    }
+
+    @Override
+    public CellViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.cell_layout, parent, false);
+
+        return new CellViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(CellViewHolder holder, int position) {
+        holder.display(documentList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return documentList.size();
+    }
+
+    public class CellViewHolder extends RecyclerView.ViewHolder {
+
+        private final ImageView icon;
+        private final TextView title;
+
+        private Document document;
+
+        public CellViewHolder(final View itemView) {
+            super(itemView);
+
+            icon = (ImageView) itemView.findViewById(R.id.imageView);
+            title = (TextView) itemView.findViewById(R.id.textView);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    new AlertDialog.Builder(itemView.getContext())
+                    .setTitle("Item clicked")
+                    .setMessage(document.getName() + " has been touched")
+                    .show();
+                }
+            });
+        }
+
+        public void display(Document document) {
+            this.document = document;
+            title.setText(document.getName());
+        }
+    }
+}
--- MyApplication/app/src/main/java/com/example/micha/permittowork/DocumentSection/DocumentsFragment.java
+++ MyApplication/app/src/main/java/com/example/micha/permittowork/DocumentSection/DocumentsFragment.java
@@ -2,27 +2,66 @@ package com.example.micha.permittowork.DocumentSection;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.micha.permittowork.Entity.Document;
+import com.example.micha.permittowork.Entity.Type;
 import com.example.micha.permittowork.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class DocumentsFragment extends Fragment {
 
+    private static final String TAG = "FragmentDocument";
+
+    private List<Document> documentList;
+
     private RecyclerView recyclerView;
+    private DividerItemDecoration itemDecoration;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         getActivity().setTitle("Documents");
+        View view = inflater.inflate(R.layout.fragment_documents, container, false);
 
-        recyclerView = (RecyclerView) getView().findViewById(R.id.recyclerView);
+        //Initialize the list of documents
+        initEntities();
+
+
+        recyclerView = (RecyclerView)  view.findViewById(R.id.document_recyclerView);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        itemDecoration = new DividerItemDecoration(recyclerView.getContext(), DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(itemDecoration);
+        recyclerView.setAdapter(new DocumentAdapter(documentList));
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_documents, container, false);
+        return view;
+    }
+
+    public void initEntities() {
+        documentList = new ArrayList<>();
+        documentList.add(new Document("Document 1", 23, Type.PDF));
+
+        documentList.add(new Document("Document 2", 11, Type.PDF));
+        documentList.add(new Document("Document 3", 98, Type.PDF));
+        documentList.add(new Document("Document 4", 56, Type.PDF));
+        documentList.add(new Document("Document 5", 77, Type.PDF));
+        documentList.add(new Document("Document 6", 242, Type.PDF));
+        documentList.add(new Document("Document 7", 128, Type.PDF));
+        documentList.add(new Document("Document 8", 34, Type.PDF));
+        documentList.add(new Document("Document 9", 99, Type.PDF));
+        documentList.add(new Document("Document 10", 62, Type.PDF));
     }
 
 }

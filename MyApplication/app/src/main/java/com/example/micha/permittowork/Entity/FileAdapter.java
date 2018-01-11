9e28e9717d8f0d5bf7755285a83879b884c4ba0d
michael.ly <lymichael94@gmail.com>
2018-01-11 14:59:43 +0100

    Add new adapter

---
+++ MyApplication/app/src/main/java/com/example/micha/permittowork/Entity/FileAdapter.java
@@ -0,0 +1,43 @@
+package com.example.micha.permittowork.Entity;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import com.example.micha.permittowork.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by micha on 11/01/2018.
+ */
+
+public class FileAdapter extends ArrayAdapter<FileBean> {
+
+    private Context context;
+    private int resources;
+    private ArrayList<FileBean> fileList;
+
+    public FileAdapter(Context context, int resource, ArrayList<FileBean> fileList) {
+        super(context, resource, fileList);
+        this.context = context;
+        this.resources = resource;
+        this.fileList = fileList;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        View view = LayoutInflater.from(context).inflate(resources, parent, false);
+        TextView textView = (TextView) view.findViewById(R.id.textFileName);
+        textView.setText(fileList.get(position).getFileName());
+
+        return view;
+    }
+}

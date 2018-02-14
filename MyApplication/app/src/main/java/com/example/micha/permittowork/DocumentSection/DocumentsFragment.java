d8b4b57b359a43fd69e43f3e9181d871942eba8e
michael.ly <lymichael94@gmail.com>
2018-02-14 10:26:20 +0100

    Add PDF viewer to document section

--- MyApplication/app/src/main/java/com/example/micha/permittowork/DocumentSection/DocumentsFragment.java
+++ MyApplication/app/src/main/java/com/example/micha/permittowork/DocumentSection/DocumentsFragment.java
@@ -1,7 +1,14 @@
 package com.example.micha.permittowork.DocumentSection;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -9,11 +16,16 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.micha.permittowork.Entity.Document;
 import com.example.micha.permittowork.Entity.Type;
+import com.example.micha.permittowork.MainActivity;
 import com.example.micha.permittowork.R;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -23,14 +35,112 @@ public class DocumentsFragment extends Fragment {
 
     private static final String TAG = "FragmentDocument";
 
+    private ListView lv_pdf;
+    public static ArrayList<File> fileList = new ArrayList<File>();
+    private PDFAdapter obj_adapter;
+    public static int REQUEST_PERMISSIONS = 1;
+    boolean boolean_permission;
+    private File dir;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         getActivity().setTitle("Documents");
         View view = inflater.inflate(R.layout.fragment_documents, container, false);
 
+        init(view);
+
         // Inflate the layout for this fragment
         return view;
     }
 
+    private void init(View view) {
+        lv_pdf = (ListView) view.findViewById(R.id.lv_pdf);
+        dir = new File(Environment.getExternalStorageDirectory().getAbsolutePath());
+        fn_permission();
+        lv_pdf.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent intent = new Intent(getActivity(), PdfActivity.class);
+                intent.putExtra("position", i);
+                startActivity(intent);
+
+                Log.e("Position", i + "");
+            }
+        });
+    }
+
+    public ArrayList<File> getfile(File dir) {
+        File listFile[] = dir.listFiles();
+        if (listFile != null && listFile.length > 0) {
+            for (int i = 0; i < listFile.length; i++) {
+
+                if (listFile[i].isDirectory()) {
+                    getfile(listFile[i]);
+
+                } else {
+
+                    boolean booleanpdf = false;
+                    if (listFile[i].getName().endsWith(".pdf")) {
+
+                        for (int j = 0; j < fileList.size(); j++) {
+                            if (fileList.get(j).getName().equals(listFile[i].getName())) {
+                                booleanpdf = true;
+                            } else {
+
+                            }
+                        }
+
+                        if (booleanpdf) {
+                            booleanpdf = false;
+                        } else {
+                            fileList.add(listFile[i]);
+
+                        }
+                    }
+                }
+            }
+        }
+        return fileList;
+    }
+
+    private void fn_permission() {
+        if ((ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)) {
+
+            if ((ActivityCompat.shouldShowRequestPermissionRationale(this.getActivity(), android.Manifest.permission.READ_EXTERNAL_STORAGE))) {
+            } else {
+                ActivityCompat.requestPermissions(this.getActivity(), new String[]{android.Manifest.permission.READ_EXTERNAL_STORAGE},
+                        REQUEST_PERMISSIONS);
+
+            }
+        } else {
+            boolean_permission = true;
+
+            getfile(dir);
+
+            obj_adapter = new PDFAdapter(getActivity(), fileList);
+            lv_pdf.setAdapter(obj_adapter);
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_PERMISSIONS) {
+
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                boolean_permission = true;
+                getfile(dir);
+
+                obj_adapter = new PDFAdapter(getActivity(), fileList);
+                lv_pdf.setAdapter(obj_adapter);
+
+            } else {
+                Toast.makeText(getActivity(), "Please allow the permission", Toast.LENGTH_LONG).show();
+
+            }
+        }
+    }
 }

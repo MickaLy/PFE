58c07d8baef5a4f45902d09c04b0540ba59cda2d
michael.ly <lymichael94@gmail.com>
2018-02-15 15:02:18 +0100

    Final version for tomorrow demonstration

--- MyApplication/app/src/main/java/com/example/micha/permittowork/DocumentSection/PdfActivity.java
+++ MyApplication/app/src/main/java/com/example/micha/permittowork/DocumentSection/PdfActivity.java
@@ -1,9 +1,14 @@
 package com.example.micha.permittowork.DocumentSection;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.micha.permittowork.R;
 import com.github.barteksc.pdfviewer.PDFView;
@@ -15,13 +20,14 @@ import com.shockwave.pdfium.PdfDocument;
 import java.io.File;
 import java.util.List;
 
-public class PdfActivity extends AppCompatActivity implements OnPageChangeListener,OnLoadCompleteListener {
+public class PdfActivity extends AppCompatActivity implements OnPageChangeListener,OnLoadCompleteListener, FloatingActionButton.OnClickListener {
     public static final String SAMPLE_FILE = "android_tutorial.pdf";
-    PDFView pdfView;
-    Integer pageNumber = 0;
-    String pdfFileName;
-    String TAG="PdfActivity";
-    int position=-1;
+    private PDFView pdfView;
+    private Integer pageNumber = 0;
+    private String pdfFileName;
+    private static final String TAG="PdfActivity";
+    private FloatingActionButton floatingButton;
+    private int position=-1;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -34,6 +40,9 @@ public class PdfActivity extends AppCompatActivity implements OnPageChangeListen
     private void init(){
         pdfView= (PDFView)findViewById(R.id.pdfView);
         position = getIntent().getIntExtra("position",-1);
+
+        floatingButton = (FloatingActionButton) findViewById(R.id.floatingActionButton);
+        floatingButton.setOnClickListener(this);
         displayFromSdcard();
     }
 
@@ -76,5 +85,28 @@ public class PdfActivity extends AppCompatActivity implements OnPageChangeListen
                 printBookmarksTree(b.getChildren(), sep + "-");
             }
         }
+    }
+
+    @Override
+    public void onClick(View v) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Demande de permission")
+        .setMessage("Vous vous apprêtez à effectuer une requête.\nVoulez-vous continuer?")
+        .setNegativeButton("Non", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //TODO
+            }
+        })
+        .setPositiveButton("Oui", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //TODO
+            }
+        }).setCancelable(false);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+
     }
 }
--- MyApplication/app/src/main/java/com/example/micha/permittowork/HomeSection/HomeFragment.java
+++ MyApplication/app/src/main/java/com/example/micha/permittowork/HomeSection/HomeFragment.java
@@ -2,20 +2,72 @@ package com.example.micha.permittowork.HomeSection;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.example.micha.permittowork.DocumentSection.DocumentsFragment;
 import com.example.micha.permittowork.R;
 
+import java.util.Calendar;
 
-public class HomeFragment extends Fragment {
+
+public class HomeFragment extends Fragment implements Button.OnClickListener {
+
+    private TextView welcome, requestWait, requestDone;
+    private Button button;
+    private String actualTime, actualDate, request_in_wait, request_done;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         getActivity().setTitle("Permit To Work");
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        init(view);
+        return view;
+    }
+
+    private void init(View view) {
+
+        welcome = (TextView) view.findViewById(R.id.welcome);
+        requestWait = (TextView) view.findViewById(R.id.request_wait);
+        requestDone = (TextView) view.findViewById(R.id.request_done);
+        button = (Button) view.findViewById(R.id.button);
+
+        actualDate = Integer.toString(Calendar.getInstance().get(Calendar.DATE)) + "/"
+                + Integer.toString(Calendar.getInstance().get(Calendar.MONTH))
+                + Integer.toString(Calendar.getInstance().get(Calendar.YEAR));
+
+        actualTime = Integer.toString(Calendar.getInstance().get(Calendar.HOUR)) + "h"
+                + Integer.toString(Calendar.getInstance().get(Calendar.MINUTE)) + "min.";
+        welcome.setText("Nous sommes le " + actualDate + ", il est " + actualTime);
+
+
+        requestWait.setText("6 requêtes en attente.");
+        requestDone.setText("11 requêtes acceptées.");
+
+        button.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Fragment fragment = null;
+        Class fragmentClass = DocumentsFragment.class;
+        try {
+            fragment = (DocumentsFragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        FragmentManager fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.content_main, fragment).commit();
     }
 }
